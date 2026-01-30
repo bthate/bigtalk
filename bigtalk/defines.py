26902e6787a0ffd0a08fef4f83b80fed232fea2f
Bart Thate <nixtniet@gmail.com>
2026-01-30 20:15:24 +0100

    add defines

---
+++ bigtalk/defines.py
@@ -0,0 +1,29 @@
+# This file is placed in the Public Domain.
+
+
+"definitions"
+
+
+from .brokers import Broker as Broker
+from .command import Commands as Commands
+from .handler import Handler as Handler
+from .handler import Client as Client
+from .handler import Console as Console
+from .handler import Output as Output
+from .message import Message as Message
+from .methods import Methods as Methods
+from .objects import Object as Object
+from .package import Mods as Mods
+from .persist import Disk as Disk
+from .persist import Util as Util
+from .persist import Locate as Locate
+from .persist import Workdir
+from .runtime import Cfg as Cfg
+from .threads import Thread as Thread
+from .utility import Log as Log
+from .utility import NoDate as NoDate
+from .utility import Repeater as Repeater
+from .utility import Time as Time
+from .utility import Timed as Timed
+from .utility import Utils as Utils
+from .utility import MONTH as MONTH
