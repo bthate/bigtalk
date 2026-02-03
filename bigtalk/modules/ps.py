fa83aaa4d0e6e123bb35397cb34d862f6793b087
Bart Thate <nixtniet@gmail.com>
2026-02-03 01:22:41 +0100

    add ps command

---
+++ bigtalk/modules/ps.py
@@ -0,0 +1,17 @@
+# This file is placed in the Public Domain.
+
+
+"show prodess stats"
+
+
+import psutil
+
+
+def ps(event):
+    proc = psutil.Process()
+    txt = ' '.join(str(proc.memory_info()).split('(')[1:])
+    txt2 = ""
+    for value in txt.split():
+        name, nrs = value.split('=')
+        txt2 += " " + f'{name.upper()}: {float(int(nrs[:-1])/1000000):.2f}M'
+    event.reply(txt2.strip())
