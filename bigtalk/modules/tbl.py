12b2c65111bde11246304cf8a32342caa9a42ffe
Nixt Niet <nixtniet@gmail.com>
2026-03-18 05:02:27 +0100

    add fields command

---
+++ bigtalk/modules/tbl.py
@@ -0,0 +1,45 @@
+# This file is placed in the Pubic Domain.
+
+
+"tables"
+
+
+NAMES = {
+    "fie": "fie",
+    "flt": "flt",
+    "fnd": "fnd",
+    "log": "log",
+    "man": "man",
+    "eml": "mbx",
+    "mbx": "mbx",
+    "dis": "mdl",
+    "now": "mdl",
+    "pth": "pth",
+    "req": "req",
+    "atr": "rss",
+    "dpl": "rss",
+    "err": "rss",
+    "exp": "rss",
+    "imp": "rss",
+    "nme": "rss",
+    "rem": "rss",
+    "res": "rss",
+    "rss": "rss",
+    "syn": "rss",
+    "lou": "sil",
+    "sil": "sil",
+    "slg": "slg",
+    "dne": "tdo",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt",
+    "wdr": "wdr",
+    "wsd": "wsd",
+    "udp": "udp"
+}
+
+
+MD5 = {
+    "mods.udp": "ce48fc590c291a7eb3ae546d75bb70d3"
+}
