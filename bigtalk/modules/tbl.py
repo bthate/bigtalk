01503a776f4f64e3028392de652bc093ae74d390
Nixt Niet <nixtniet@gmail.com>
2026-03-18 05:32:03 +0100

    update table

--- bigtalk/modules/tbl.py
+++ bigtalk/modules/tbl.py
@@ -5,6 +5,17 @@
 
 
 NAMES = {
+    "atr": "rss",
+    "dpl": "rss",
+    "err": "rss",
+    "exp": "rss",
+    "imp": "rss",
+    "nme": "rss",
+    "rem": "rss",
+    "res": "rss",
+    "rss": "rss",
+    "syn": "rss",
+    "thr": "thr",
     "fie": "fie",
     "flt": "flt",
     "fnd": "fnd",
@@ -16,30 +27,39 @@ NAMES = {
     "now": "mdl",
     "pth": "pth",
     "req": "req",
-    "atr": "rss",
-    "dpl": "rss",
-    "err": "rss",
-    "exp": "rss",
-    "imp": "rss",
-    "nme": "rss",
-    "rem": "rss",
-    "res": "rss",
-    "rss": "rss",
-    "syn": "rss",
     "lou": "sil",
     "sil": "sil",
     "slg": "slg",
     "dne": "tdo",
     "tdo": "tdo",
-    "thr": "thr",
     "tmr": "tmr",
+    "udp": "udp",
     "upt": "upt",
     "wdr": "wdr",
-    "wsd": "wsd",
-    "udp": "udp"
+    "wsd": "wsd"
 }
 
 
 MD5 = {
-    "mods.udp": "ce48fc590c291a7eb3ae546d75bb70d3"
+    "bigtalk.modules.atr": "12a7dc7789a921d759f278d8782137f3",
+    "bigtalk.modules.fie": "9adc739cbeaccb0bad39a5db81f8bd0b",
+    "bigtalk.modules.flt": "9d19021fb68edf998d801197982c081b",
+    "bigtalk.modules.fnd": "a0951661a39c633445addbdf945b0cd0",
+    "bigtalk.modules.log": "dd6094da8a62dea4ee27979506f76ccc",
+    "bigtalk.modules.man": "b748396cfcf563b0f06df1546c930fce",
+    "bigtalk.modules.mbx": "b58b13f36afddc6290404b7fca15265c",
+    "bigtalk.modules.mdl": "d561fa8884ac141e41d7ce081991cc58",
+    "bigtalk.modules.pth": "ac2b5aa3e5b8a1d9810585cdda60e940",
+    "bigtalk.modules.req": "bc1984d2e9de0310dc1b468f25c7ab8c",
+    "bigtalk.modules.rst": "10fd06237348c3fbf0418b1be2442677",
+    "bigtalk.modules.sil": "6927ad118995f9353a3ba867555024aa",
+    "bigtalk.modules.slg": "0196cecb8ff67e032bd389a0b1860353",
+    "bigtalk.modules.tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "bigtalk.modules.tdo": "a2ac6e6df8566a61997b46db29f6760d",
+    "bigtalk.modules.tmr": "ccc775fc3e71171ba407676337e48a8f",
+    "bigtalk.modules.udp": "e01089c672b815fe90ab55125e90b8de",
+    "bigtalk.modules.upt": "0c899c354b57265cb100a3b38a8668a1",
+    "bigtalk.modules.wdr": "0e0492203ff32017137bd4218fb10018",
+    "bigtalk.modules.web": "e6aec2e213cb74eae1fd14ca709c4e06",
+    "bigtalk.modules.wsd": "9c509f9cca61d55cfa3983abfbdb2286"
 }
