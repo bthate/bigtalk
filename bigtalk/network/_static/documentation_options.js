5f5edec662974339b98d195b4a378c843ad024bc
Nixt Niet <nixtniet@gmail.com>
2026-05-11 18:56:26 +0200

    update package

--- bigtalk/network/_static/documentation_options.js
+++ bigtalk/network/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '189',
+    VERSION: '234',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
