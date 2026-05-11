e95f7d219d2b935ec2dae15b01452ff318d50262
Nixt Niet <nixtniet@gmail.com>
2026-05-11 18:56:45 +0200

    update package

---
+++ bigtalk/modules/flt.py
@@ -0,0 +1,22 @@
+# This file is placed in the Public Domain.
+
+
+"show bots in fleet"
+
+
+from bigtalk.defines import Broker, Method
+
+
+def flt(event):
+    clts = list(Broker.objs("announce"))
+    if not clts:
+        event.reply("no bots")
+        return
+    if event.args:
+        index = int(event.args[0])
+        if index < len(clts):
+            event.reply(str(clts[index]))
+        else:
+            event.reply("no matching client in fleet.")
+        return
+    event.reply(' | '.join([Method.fqn(o).split(".")[-1] for o in clts]))
---
+++ testing/test_methods.py
@@ -0,0 +1,19 @@
+# This file is placed in the Public Domain.
+
+
+"logging tests"
+
+
+import unittest
+
+
+from bigtalk.objects import Base, Method
+
+
+class TestMethod(unittest.TestCase):
+
+    def test_clear(self):
+        obj = Base()
+        obj.a = "b"
+        Method.clear(obj)
+        self.assertEqual(str(obj), "{}")
